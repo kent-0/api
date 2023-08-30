24103c3b7e6c41f1bf3a1877348a4a45acf05298
Sawako <56084970+sawa-ko@users.noreply.github.com>
2023-08-29 22:57:34 -0600

    chore: add perfectionist eslint plugin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-
 import { join } from 'path';
+
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'scheme.graphql'),
       driver: ApolloDriver,
       playground: true,
-      autoSchemaFile: join(process.cwd(), 'scheme.graphql'),
       sortSchema: true,
     }),
     UserModule,
