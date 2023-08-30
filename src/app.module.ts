3126237a0c4868a7b5f0450a554afbd284f0ea86
Sawako <56084970+sawa-ko@users.noreply.github.com>
2023-08-29 23:34:44 -0600

    feat: add mikro-orm and missing entities remaining

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 
@@ -7,12 +10,32 @@ import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
+    // Extensions
+    ConfigModule.forRoot({
+      cache: true,
+      envFilePath: '.env.local',
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: join(process.cwd(), 'scheme.graphql'),
       driver: ApolloDriver,
       playground: true,
       sortSchema: true,
     }),
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (_configService: ConfigService) => ({
+        autoLoadEntities: true,
+        dbName: _configService.get<string>('DB_NAME', { infer: true }),
+        driver: PostgreSqlDriver,
+        host: _configService.get<string>('DB_HOST', { infer: true }),
+        password: _configService.get<string>('DB_PASS', { infer: true }),
+        user: _configService.get<string>('DB_USER', { infer: true }),
+      }),
+    }),
+
+    // App modules
     UserModule,
   ],
 })
