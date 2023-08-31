f18655b00795054e92da8161160ecb5d9f9eefd1
Sawako <56084970+sawa-ko@users.noreply.github.com>
2023-08-30 22:04:12 -0600

    feat: use ts mikro-orm config and create initial migration

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { PostgreSqlDriver, defineConfig } = require('@mikro-orm/postgresql');
+import { PostgreSqlDriver, defineConfig } from '@mikro-orm/postgresql';
 
-exports.default = defineConfig({
+export default defineConfig({
   driver: PostgreSqlDriver,
   entities: ['./dist/database/entities'],
   entitiesTs: ['./src/database/entities'],
