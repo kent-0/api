ba9bad1ca2723519d74d155243fb9e94695552f6
Sawako <56084970+sawa-ko@users.noreply.github.com>
2023-08-30 22:21:01 -0600

    fix(database): migrations and user entitiy

--- src/database/entities/base.ts
+++ src/database/entities/base.ts
@@ -1,13 +1,25 @@
 import { BaseEntity, Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity({ abstract: true, comment: 'Default configuration of all entities.' })
-export abstract class ParentEntity extends BaseEntity<ParentEntity, 'uuid'> {
+export abstract class ParentEntity extends BaseEntity<ParentEntity, 'id'> {
   @Property({
     columnType: 'date',
     comment: 'Date when the row was created.',
     type: 'date',
   })
   public createdAt = new Date();
+
+  /**
+   * Required postgres extension
+   * uuid-ossp
+   * @see https://www.postgresql.org/docs/current/uuid-ossp.html
+   */
+  @PrimaryKey({
+    comment: 'UUID v4 unique to the row.',
+    defaultRaw: 'uuid_generate_v4()',
+    type: 'uuid',
+  })
+  public id: number;
 
   @Property({
     columnType: 'date',
@@ -16,19 +28,6 @@ export abstract class ParentEntity extends BaseEntity<ParentEntity, 'uuid'> {
     type: 'date',
   })
   public updatedAt = new Date();
-
-  /**
-   * Required postgres extension
-   * uuid-ossp
-   * @see https://www.postgresql.org/docs/current/uuid-ossp.html
-   */
-  @PrimaryKey({
-    columnType: 'text',
-    comment: 'UUID v4 unique to the row.',
-    defaultRaw: 'uuid_generate_v4()',
-    type: 'uuid',
-  })
-  public uuid: number;
 
   @Property({ comment: 'Optimistic Locking transactions.', version: true })
   public version: number;
--- src/database/migrations/Migration20230831041831.ts
+++ src/database/migrations/Migration20230831041831.ts
@@ -1,10 +1,10 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20230831041254 extends Migration {
+export class Migration20230831041831 extends Migration {
 
   async up(): Promise<void> {
-    this.addSql('create table "auth_users" ("uuid" text not null default uuid_generate_v4(), "created_at" date not null, "updated_at" date not null, "version" int not null default 1, "email" varchar not null, "first_name" varchar not null, "last_name" varchar not null, "username" varchar not null, constraint "auth_users_pkey" primary key ("uuid"), constraint auth_users_username_check check (username ~ \'^[A-Za-z0-9_-]+$\'));');
-    this.addSql('comment on column "auth_users"."uuid" is \'UUID v4 unique to the row.\';');
+    this.addSql('create table "auth_users" ("id" uuid not null default uuid_generate_v4(), "created_at" date not null, "updated_at" date not null, "version" int not null default 1, "email" varchar not null, "first_name" varchar not null, "last_name" varchar not null, "username" varchar not null, constraint "auth_users_pkey" primary key ("id"), constraint auth_users_username_check check (username ~ \'^[A-Za-z0-9_-]+$\'));');
+    this.addSql('comment on column "auth_users"."id" is \'UUID v4 unique to the row.\';');
     this.addSql('comment on column "auth_users"."created_at" is \'Date when the row was created.\';');
     this.addSql('comment on column "auth_users"."updated_at" is \'Date when the row was last updated.\';');
     this.addSql('comment on column "auth_users"."version" is \'Optimistic Locking transactions.\';');
