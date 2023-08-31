01c77372f19f17a061b3212991a591c79690e781
Sawako <56084970+sawa-ko@users.noreply.github.com>
2023-08-31 11:22:35 -0600

    feat(database): comment enum token type

--- src/database/enums/token.enum.ts
+++ src/database/enums/token.enum.ts
@@ -1,4 +1,14 @@
+/**
+ * Types of tokens available for user accounts.
+ */
 export enum TokenType {
+  /**
+   * Authorization to access the account.
+   */
   AUTH = 'auth',
+
+  /**
+   * Refresh token for the authentication type token.
+   */
   REFRESH = 'refresh',
 }
