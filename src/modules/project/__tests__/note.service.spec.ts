a96751e40e1bc1d5c679d8774deadd6abf6c7c47
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2023-10-02 17:02:02 -0600

    feat(tests): add fully tests for project notes service

--- src/modules/project/__tests__/note.service.spec.ts
+++ src/modules/project/__tests__/note.service.spec.ts
@@ -11,32 +11,31 @@ import {
   AuthTokensEntity,
   AuthUserEntity,
   ProjectEntity,
-  ProjectGoalsEntity,
   ProjectMembersEntity,
+  ProjectNotesEntity,
 } from '~/database/entities';
 import { AuthModule } from '~/modules/auth/auth.module';
 import { AuthAccountService } from '~/modules/auth/services/account.service';
-import { ProjectGoalService } from '~/modules/project/services/goal.service';
-import { ProjectMemberService } from '~/modules/project/services/member.service';
+import { ProjectNoteService } from '~/modules/project/services/note.service';
 import { ProjectService } from '~/modules/project/services/project.service';
 
 import { TestingMikroORMConfig } from '../../../../mikro-orm.config';
 
 /**
- * `Goals Successful Cases` Test Suite:
- * This test suite focuses on validating the successful scenarios related to project goals.
- * The primary goal is to ensure that the system correctly handles valid operations or data
- * related to goals and provides expected outcomes.
+ * `Notes Successful Cases` Test Suite:
+ * This test suite focuses on validating the successful scenarios related to project notes.
+ * The primary note is to ensure that the system correctly handles valid operations or data
+ * related to notes and provides expected outcomes.
  */
-describe('Goals successfully cases', async () => {
-  let service: ProjectGoalService;
+describe('Notes successfully cases', async () => {
+  let service: ProjectNoteService;
   let accountService: AuthAccountService;
-  let projectMemberService: ProjectMemberService;
   let projectService: ProjectService;
   let module: TestingModule;
   let em: EntityManager;
   let orm: MikroORM;
   let project: ProjectEntity;
+  let user: AuthUserEntity;
 
   /**
    * Before All Setup:
@@ -65,26 +64,24 @@ describe('Goals successfully cases', async () => {
             AuthTokensEntity,
             AuthEmailsEntity,
             ProjectEntity,
-            ProjectGoalsEntity,
+            ProjectNotesEntity,
           ],
         }),
         AuthModule,
       ],
-      providers: [ProjectGoalService, ProjectMemberService, ProjectService],
+      providers: [ProjectNoteService, ProjectService],
     }).compile();
 
-    service = module.get<ProjectGoalService>(ProjectGoalService);
+    service = module.get<ProjectNoteService>(ProjectNoteService);
     orm = module.get<MikroORM>(MikroORM);
     em = module.get<EntityManager>(EntityManager);
     accountService = module.get<AuthAccountService>(AuthAccountService);
     projectService = module.get<ProjectService>(ProjectService);
-    projectMemberService =
-      module.get<ProjectMemberService>(ProjectMemberService);
 
     await orm.getSchemaGenerator().refreshDatabase();
 
     await RequestContext.createAsync(em, async () => {
-      const userProjectOwner = await accountService.signUp({
+      const userTest = await accountService.signUp({
         email: 'sawa@acme.com',
         first_name: 'Sawa',
         last_name: 'Ko',
@@ -92,28 +89,16 @@ describe('Goals successfully cases', async () => {
         username: 'sawako',
       });
 
-      const userMemberTest = await accountService.signUp({
-        email: 'sawa2@acme.com',
-        first_name: 'Sawa',
-        last_name: 'Ko',
-        password: 'sawako',
-        username: 'sawako2',
-      });
-
       const projectTest = await projectService.create(
         {
           description: 'Kento testing project',
           name: 'Kento',
         },
-        userProjectOwner.id,
+        userTest.id,
       );
 
       project = await em.findOneOrFail(ProjectEntity, { id: projectTest.id });
-
-      await projectMemberService.add({
-        projectId: project.id,
-        userId: userMemberTest.id,
-      });
+      user = await em.findOneOrFail(AuthUserEntity, { id: userTest.id });
     });
   });
 
@@ -126,92 +111,109 @@ describe('Goals successfully cases', async () => {
   });
 
   /**
-   * Test Case: Goal Creation:
-   * Validates that a new goal can be successfully created.
+   * Test Case: Note Creation:
+   * Validates that a new note can be successfully created.
    */
-  it('should create a goal', async () => {
+  it('should create a note', async () => {
     await RequestContext.createAsync(em, async () => {
-      const goal = await service.create({
-        description: 'Kento testing goal',
-        name: 'Kento',
-        projectId: project.id,
-      });
+      const note = await service.create(
+        {
+          content: 'Kento testing note',
+          projectId: project.id,
+          title: 'Kento',
+        },
+        user.id,
+      );
 
-      expect(goal).toBeDefined();
-      expect(goal.id).toBeDefined();
-      expect(goal.description).toEqual('Kento testing goal');
-      expect(goal.name).toEqual('Kento');
+      expect(note).toBeDefined();
+      expect(note.id).toBeDefined();
+      expect(note.content).toEqual('Kento testing note');
+      expect(note.title).toEqual('Kento');
+      expect(note.created_by.id).toEqual(user.id);
     });
   });
 
   /**
-   * Test Case: Goal Update:
-   * Validates that an existing goal can be successfully updated with new details.
+   * Test Case: Note Update:
+   * Validates that an existing note can be successfully updated with new details.
    * Expected Outcome:
-   * - The goal entity should reflect the new details after the update.
+   * - The note entity should reflect the new details after the update.
    */
-  it('should update a goal', async () => {
+  it('should update a note', async () => {
     await RequestContext.createAsync(em, async () => {
-      const goal = await service.create({
-        description: 'Kento testing goal',
-        name: 'Kento',
-        projectId: project.id,
-      });
+      const note = await service.create(
+        {
+          content: 'Kento testing project',
+          projectId: project.id,
+          title: 'Kento',
+        },
+        user.id,
+      );
 
-      const updatedGoal = await service.update({
-        description: 'Kento testing goal updated',
-        goalId: goal.id,
-        name: 'Kento updated',
-        projectId: project.id,
-      });
+      const updatedNote = await service.update(
+        {
+          content: 'Kento testing note updated',
+          noteId: note.id,
+          projectId: project.id,
+          title: 'Kento updated',
+        },
+        user.id,
+      );
 
-      expect(updatedGoal).toBeDefined();
-      expect(updatedGoal.id).toBeDefined();
-      expect(updatedGoal.description).toEqual('Kento testing goal updated');
-      expect(updatedGoal.name).toEqual('Kento updated');
+      expect(updatedNote).toBeDefined();
+      expect(updatedNote.id).toBeDefined();
+      expect(updatedNote.content).toEqual('Kento testing note updated');
+      expect(updatedNote.title).toEqual('Kento updated');
+      expect(updatedNote.created_by.id).toEqual(user.id);
     });
   });
 
   /**
-   * Test Case: Goal Deletion:
-   * Validates that an existing goal can be successfully deleted from a project.
+   * Test Case: Note Deletion:
+   * Validates that an existing note can be successfully deleted from a project.
    * Expected Outcome:
-   * - The goal entity should no longer exist in the database.
+   * - The note entity should no longer exist in the database.
    * - A confirmation message should be returned indicating successful deletion.
    */
-  it('should delete a goal', async () => {
+  it('should delete a note', async () => {
     await RequestContext.createAsync(em, async () => {
-      const goal = await service.create({
-        description: 'Kento testing goal',
-        name: 'Kento',
-        projectId: project.id,
-      });
+      const note = await service.create(
+        {
+          content: 'Kento testing project',
+          projectId: project.id,
+          title: 'Kento',
+        },
+        user.id,
+      );
 
-      const deletedGoal = await service.delete({
-        goalId: goal.id,
-        projectId: project.id,
-      });
+      const deletedNote = await service.delete(
+        {
+          noteId: note.id,
+          projectId: project.id,
+        },
+        user.id,
+      );
 
-      expect(deletedGoal).toBe('The goal has been successfully removed.');
+      expect(deletedNote).toBe('The note has been successfully removed.');
     });
   });
 });
 
 /**
- * `Goals Unsuccessful Cases` Test Suite:
- * This test suite focuses on validating the unsuccessful scenarios related to project goals.
- * The primary goal is to ensure that the system correctly handles invalid operations or data
- * related to goals and provides expected outcomes.
+ * `Notes Unsuccessful Cases` Test Suite:
+ * This test suite focuses on validating the unsuccessful scenarios related to project notes.
+ * The primary note is to ensure that the system correctly handles invalid operations or data
+ * related to notes and provides expected outcomes.
  */
-describe('Goals unsuccessfully cases', async () => {
-  let service: ProjectGoalService;
+describe('Notes unsuccessfully cases', async () => {
+  let service: ProjectNoteService;
   let accountService: AuthAccountService;
-  let projectMemberService: ProjectMemberService;
   let projectService: ProjectService;
   let module: TestingModule;
   let em: EntityManager;
   let orm: MikroORM;
   let project: ProjectEntity;
+  let user: AuthUserEntity;
 
   /**
    * Before All Setup:
@@ -240,26 +242,24 @@ describe('Goals unsuccessfully cases', async () => {
             AuthTokensEntity,
             AuthEmailsEntity,
             ProjectEntity,
-            ProjectGoalsEntity,
+            ProjectNotesEntity,
           ],
         }),
         AuthModule,
       ],
-      providers: [ProjectGoalService, ProjectMemberService, ProjectService],
+      providers: [ProjectNoteService, ProjectService],
     }).compile();
 
-    service = module.get<ProjectGoalService>(ProjectGoalService);
+    service = module.get<ProjectNoteService>(ProjectNoteService);
     orm = module.get<MikroORM>(MikroORM);
     em = module.get<EntityManager>(EntityManager);
     accountService = module.get<AuthAccountService>(AuthAccountService);
     projectService = module.get<ProjectService>(ProjectService);
-    projectMemberService =
-      module.get<ProjectMemberService>(ProjectMemberService);
 
     await orm.getSchemaGenerator().refreshDatabase();
 
     await RequestContext.createAsync(em, async () => {
-      const userProjectOwner = await accountService.signUp({
+      const userTest = await accountService.signUp({
         email: 'sawa@acme.com',
         first_name: 'Sawa',
         last_name: 'Ko',
@@ -267,28 +267,16 @@ describe('Goals unsuccessfully cases', async () => {
         username: 'sawako',
       });
 
-      const userMemberTest = await accountService.signUp({
-        email: 'sawa2@acme.com',
-        first_name: 'Sawa',
-        last_name: 'Ko',
-        password: 'sawako',
-        username: 'sawako2',
-      });
-
       const projectTest = await projectService.create(
         {
           description: 'Kento testing project',
           name: 'Kento',
         },
-        userProjectOwner.id,
+        userTest.id,
       );
 
       project = await em.findOneOrFail(ProjectEntity, { id: projectTest.id });
-
-      await projectMemberService.add({
-        projectId: project.id,
-        userId: userMemberTest.id,
-      });
+      user = await em.findOneOrFail(AuthUserEntity, { id: userTest.id });
     });
   });
 
@@ -301,37 +289,43 @@ describe('Goals unsuccessfully cases', async () => {
   });
 
   /**
-   * Test Case: Goal Update:
-   * Validates that a goal cannot be updated if the goal does not exist.
+   * Test Case: Note Update:
+   * Validates that a note cannot be updated if the note does not exist.
    */
-  it('should not update a goal if the goal does not exist', async () => {
+  it('should not update a note if the note does not exist', async () => {
     await RequestContext.createAsync(em, async () => {
       await expect(
-        service.update({
-          description: 'Kento testing goal',
-          goalId: '8054de11-b6dc-481e-a8c2-90cef8169914',
-          name: 'Kento',
-          projectId: project.id,
-        }),
+        service.update(
+          {
+            content: 'Kento testing note',
+            noteId: '8054de11-b6dc-481e-a8c2-90cef8169914',
+            projectId: project.id,
+            title: 'Kento',
+          },
+          user.id,
+        ),
       ).rejects.toThrowError(
-        'The project goal you are trying to update could not be found.',
+        'The project note you are trying to update could not be found.',
       );
     });
   });
 
   /**
-   * Test Case: Goal Deletion:
-   * Validates that a goal cannot be deleted if the goal does not exist.
+   * Test Case: Note Deletion:
+   * Validates that a note cannot be deleted if the note does not exist.
    */
-  it('should not delete a goal if the goal does not exist', async () => {
+  it('should not delete a note if the note does not exist', async () => {
     await RequestContext.createAsync(em, async () => {
       await expect(
-        service.delete({
-          goalId: '8054de11-b6dc-481e-a8c2-90cef8169914',
-          projectId: project.id,
-        }),
+        service.delete(
+          {
+            noteId: '8054de11-b6dc-481e-a8c2-90cef8169914',
+            projectId: project.id,
+          },
+          user.id,
+        ),
       ).rejects.toThrowError(
-        'The project goal you are trying to delete could not be found.',
+        'The project note you are trying to delete was not found.',
       );
     });
   });
